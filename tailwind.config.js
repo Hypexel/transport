558c570ec7cb2b635b610d2f06f10e12d4406f2a
GrandpaTchia <jasbinderk235@gmail.com>
2026-03-28 17:01:28 +0530

    Update and rename App.jsx to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandDark: '#0b1120',
+        brandBlue: '#1d4ed8',
+        brandGreen: '#059669'
+      }
+    },
+  },
+  plugins: [],
+}
